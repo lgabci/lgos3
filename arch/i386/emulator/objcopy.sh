9d46694e8cb8a4e2c1fe5d089d861dea62ebd973
lgabci <gl12qw@gmail.com>
2019-01-05 20:11:16 +0100

    24

--- arch/i386/emulator/objcopy.sh
+++ arch/i386/emulator/objcopy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 IMG="$1"
 ELF="$2"
@@ -8,18 +9,21 @@ if [[ $($AWK --version 2>/dev/null) = GNU* ]]; then
   AWKFLAGS=--non-decimal-data
 fi
 
-
 AWKPROG='/^Idx/,0 {
   if ($1 ~ /[0-9]+/) {
-    size = sprintf("%d", "0x" $3);
+    count = sprintf("%d", "0x" $3);
     seek = sprintf("%d", "0x" $5);
     skip = sprintf("%d", "0x" $6);
     getline;
     if (/ ALLOC/ && / LOAD/) {
-      print skip seek size
+      print skip " " seek " " count
     }
   }
 }'
 
 $OBJDUMP -h "$ELF"
-$OBJDUMP -h "$ELF" | $AWK $AWKFLAGS "$AWKPROG" -
+$OBJDUMP -h "$ELF" |
+  $AWK $AWKFLAGS "$AWKPROG" - |
+  while read skip seek count; do
+    $DD if=$ELF of=$IMG bs=1 skip=$skip seek=$seek count=$count conv=notrunc
+  done
