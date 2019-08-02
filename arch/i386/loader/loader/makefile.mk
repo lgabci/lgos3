dfe4b35ef0d40feec86072833e5fd9b7a7caf891
lgabci <gl12qw@gmail.com>
2019-08-02 23:52:32 +0200

    7

--- arch/i386/loader/loader/makefile.mk
+++ arch/i386/loader/loader/makefile.mk
@@ -1,10 +1,5 @@
 # i386 boot loader makefile
 
-#CCFLAGS += -m16
-
-#loader.elf: init.o console.o disk.o elf.o ext2.o fat.o lib.o loader.o \
-             multiboot.o
-
 .PHONY: bootblock
 bootblock: $(DESTDIR)/loader.elf
 
@@ -13,6 +8,8 @@ bootblock: $(DESTDIR)/loader.elf
 OBJS := $(DESTDIR)/init.o $(DESTDIR)/console.o \
   $(DESTDIR)/disk.o $(DESTDIR)/elf.o $(DESTDIR)/ext2.o $(DESTDIR)/fat.o \
   $(DESTDIR)/lib.o $(DESTDIR)/loader.o $(DESTDIR)/multiboot.o
+
+$(OBJS): CCFLAGS += -m16
 
 $(OBJS): | $(DESTDIR)
 
