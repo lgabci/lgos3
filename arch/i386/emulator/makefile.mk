56e78a81b6d6bd077f5dec7e3c19d035f3224f9c
lgabci <gl12qw@gmail.com>
2019-09-12 19:41:10 +0200

    11

--- arch/i386/emulator/makefile.mk
+++ arch/i386/emulator/makefile.mk
@@ -16,23 +16,30 @@ PFDISK := o n p 1 $(PSTART) +$(PSIZE) w
 PLOSETUP := -o $$(($(PSTART) * $(HDSECSIZE))) \
 --sizelimit $$(($(PSIZE) * $(HDSECSIZE))) --sector-size $(HDSECSIZE)
 
+PQEMU = -machine pc -cpu 486 -boot order=c -m size=2 \
+-drive file=$<,if=ide,index=0,media=disk,format=raw -nic none
+
 .PHONY: emulator
-emulator: $(HDIMG).fs
+emulator: qemu
 
+.PHONY: qemu
+qemu: $(HDIMG)
+	qemu-system-$(ARCH) $(PQEMU)
 
-## TODO $(HDIMG): $(LOADER_ELF) $(KERNEL_ELF) $(BOOTBLOCK_EXT2_ELF) | $(DESTDIR)
-
-$(HDIMG).fs: $(HDIMG).losetup
-	sudo mkfs.ext2 $$(cat $<)
-	touch $@
-
-$(HDIMG).losetup: $(HDIMG).part
-	sudo losetup $(PLOSETUP) --show -f $(@:.losetup=) >$@
-
-$(HDIMG).part: $(HDIMG)
-	echo $(PFDISK) | tr ' ' '\n' | /usr/sbin/fdisk $< >/dev/null
-	mkdir -p $@
-	touch $@
-
-$(HDIMG): | $(DESTDIR)
+## BOOTBLOCK_FAT_ELF
+$(HDIMG): $(BOOTBLOCK_MBR_ELF) $(BOOTBLOCK_EXT2_ELF) $(LOADER_ELF) | $(DESTDIR)
 	dd if=/dev/zero of=$@ bs=1 seek=$(HDSIZE) count=0 status=none
+	echo $(PFDISK) | tr ' ' '\n' | /usr/sbin/fdisk $@ >/dev/null
+	echo CREATE MBR ## TODO
+	sudo losetup $(PLOSETUP) --show -f $@ >$@.losetup
+	sudo mkfs.ext2 $$(cat $@.losetup) >/dev/null
+	echo CREATE BOOT ## TODO
+	mkdir -p $@.mount
+	sudo mount $$(cat $@.losetup) $@.mount
+	sudo mkdir -p $@.mount/loader
+	sudo chown $$USER:$$USER $@.mount/loader
+	cp $(word 3,$^) $@.mount/loader/
+	sudo umount $$(cat $@.losetup)
+	sudo losetup -d $$(cat $@.losetup)
+	rmdir $@.mount
+	rm $@.losetup
